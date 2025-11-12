0faeeb14c444666e7b2a18db04d87459ac02d74a
Vinaytarigoppula <vinaytarigoppula@gmail.com>
2025-11-12 20:04:04 +0530

    third question

---
+++ imple/Beautiful_Matrix.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    vector<vector<int>> arr(5,vector<int> (5));
+    int r,c;
+    for(int i=0;i<5;i++)
+    {
+        for(int j=0;j<5;j++)
+        {
+            cin>>arr[i][j];
+            if(arr[i][j]==1)
+            {
+                r=i;
+                c=j;
+            }
+        }
+    }
+    int res=abs(2-r)+abs(2-c);
+    cout<<res;
+}
